795016f660c49ccea73f39b64cbe153b57517895
reesealanj <reesealanj@gwu.edu>
2020-07-06 13:49:40 -0700

    Added base for p5.js sketch

---
+++ README.md
@@ -0,0 +1,3 @@
+# Genetic Rockets
+
+This project is an attempt to implement an evolutionary style genetic algorithm in javascript and visualize it's function using p5.javascript
---
+++ js/sketch.js
@@ -0,0 +1,9 @@
+let canvas;
+
+function setup() {
+  canvas = createCanvas(500, 500);
+  // set div with id 'p5-canvas' as the place for the canvas
+  canvas.parent("p5-canvas");
+}
+
+function draw() {}
